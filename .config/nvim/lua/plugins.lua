55f1ac0b757eeceef12e0b9387b5b9e6a542d226
Zhiyang He <hezhiyang@mail.ustc.edu.cn>
2022-09-07 22:48:13 +0800

    提交了重要的dotfile

---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,16 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+  use { "williamboman/mason.nvim" }
+  use "williamboman/mason-lspconfig.nvim"
+  use 'neovim/nvim-lspconfig'
+  use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
+
+  
+end)
+
