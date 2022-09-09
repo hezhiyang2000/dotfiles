39084016a1e3e9abcbaeca6c86b5a9f7607371cc
Zhiyang He <hezhiyang@mail.ustc.edu.cn>
2022-09-09 16:38:08 +0800

    更改了bspwm的相关设置，使得显示器热插拔成为可能

--- .config/bspwm/bspwm_setup_monitors.sh
+++ .config/bspwm/bspwm_setup_monitors.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
+export DISPLAY=:0
+
 INTERNAL_MONITOR="eDP-1"
 EXTERNAL_MONITOR="DP-1"
 
 monitor_add() {
   # Move first 5 desktops to external monitor
   for desktop in $(bspc query -D --names -m "$INTERNAL_MONITOR" | sed 5q); do
-    bspc desktop "$desktop" --to-monitor "$EXTERNAL_MONITOR"
+    bspc desktop "$desktop" --to-monitor "$EXTERNAL_MONITOR" >>/var/tmp/add.log
   done
   # Remove default desktop created by bspwm
   bspc desktop Desktop --remove
@@ -24,13 +26,13 @@ monitor_remove() {
 	done
 
   # delete default desktops
-  bspc desktop Desktop --remove
+  echo "delete" >/var/tmp/delete.log
+  bspc monitor "$EXTERNAL_MONITOR" --remove
   # reorder desktops
   bspc monitor "$INTERNAL_MONITOR" -o 1 2 3 4 5 6 7 8 9 10
 }
 
 if [[ $(xrandr -q | grep "^${EXTERNAL_MONITOR} connected") ]]; then
-  echo "connected"
   # set xrandr rules for docked setup
   xrandr --output "$EXTERNAL_MONITOR" --mode 1920x1080 --pos 0x0 --rotate normal --output "$INTERNAL_MONITOR" --primary --mode 1920x1080 --pos 1920x0 --rotate normal
   if [[ $(bspc query -D -m "${EXTERNAL_MONITOR}" | wc -l) -ne 5 ]]; then
@@ -38,7 +40,6 @@ if [[ $(xrandr -q | grep "^${EXTERNAL_MONITOR} connected") ]]; then
   fi
   bspc wm -O "$EXTERNAL_MONITOR" "$INTERNAL_MONITOR"
 else
-  echo "disconnected"
   # set xrandr rules for mobile setup
   xrandr --output "$INTERNAL_MONITOR" --primary --mode 1920x1080 --pos 0x0 --rotate normal --output "$EXTERNAL_MONITOR" --off
   if [[ $(bspc query -D -m "${INTERNAL_MONITOR}" | wc -l) -ne 10 ]]; then
@@ -52,10 +53,11 @@ feh --no-fehbg --bg-scale ~/images/background/
 # Kill and relaunch polybar
 POLYDIR="/home/candy/.config/polybar/forest"
 killall -q polybar
-while pgrep -u $UID -x polybar > /dev/null; do sleep 2; done
-if [[ $(xrandr -q | grep "${EXTERNAL_MONITOR} connected") ]]; then
-  polybar --reload main -c "$POLYDIR"/config.ini &
-  polybar --reload side -c "$POLYDIR"/config.ini &
+while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
+if [[ $(xrandr -q | grep "^${EXTERNAL_MONITOR} connected") ]]; then
+  echo "polybar two" >/var/tmp/two.log
+  (polybar --reload side -c "$POLYDIR"/config.ini  </dev/null >/var/tmp/polybar-side.log 2>&1 &)
+  (polybar --reload main -c "$POLYDIR"/config.ini  </dev/null >/var/tmp/polybar-main.log 2>&1 &)
 else
-  polybar --reload main -c "$POLYDIR"/config.ini &
+  (polybar --reload main -c "$POLYDIR"/config.ini  </dev/null >/var/tmp/polybar-main.log 2>&1 &)
 fi
